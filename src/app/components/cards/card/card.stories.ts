84834b857c58adb3097f892b2c4ac3eb5ed3ef54
Evyweb <dev@evyweb.fr>
2023-03-01 13:41:23 +0100

    feat(card): Add basic card component

---
+++ src/app/components/cards/card/card.stories.ts
@@ -0,0 +1,26 @@
+import {CardComponent} from "./card.component";
+import {Meta, moduleMetadata, Story} from "@storybook/angular";
+import {CommonModule} from "@angular/common";
+import {NgHeroiconsModule} from "@dimaslz/ng-heroicons";
+
+export default {
+  title: 'Cards/Card',
+  component: CardComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [
+        CommonModule,
+        NgHeroiconsModule
+      ],
+    })
+  ],
+  args: {
+  }
+} as Meta<CardComponent>;
+
+const baseTemplate: Story = (args) => ({
+  props: args,
+});
+
+export const Default = baseTemplate.bind({});
+
