e8e4b0117d2fbe70c2c391395b3600809e4a6b1f
Evyweb <dev@evyweb.fr>
2023-03-02 22:12:46 +0100

    refactor(navbar): Move navbar to navigation folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {NavbarComponent} from './components/cards/navbar/navbar.component';
+import {NavbarComponent} from './components/navigation/navbar/navbar.component';
 import {NgHeroiconsModule} from "@dimaslz/ng-heroicons";
 
 @NgModule({
